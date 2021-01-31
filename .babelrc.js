a717504287d9166ab9c0d21eeec71f1ddecc85ee
Tymon <tomasz.mojsiuk@gmail.com>
2021-02-01 00:10:52 +0100

    update babelrs.js for jest to operato on modules

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,10 @@
 const isProd = String(process.env.NODE_ENV) === 'production'
+// DESC add isTest check for compiling moduled while testing
+const isTest = String(process.env.NODE_ENV) === 'test'
 
 module.exports = {
   presets: [
-    ['@babel/preset-env', {modules: false}],
+    ['@babel/preset-env', {modules: isTest ? 'commonjs' : false}],
     '@babel/preset-react',
     [
       '@emotion/babel-preset-css-prop',
