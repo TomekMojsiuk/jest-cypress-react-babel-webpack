72be7ac865bf51876449904ab622e21b3c5da6a2
Tymon <tomasz.mojsiuk@gmail.com>
2021-02-05 10:36:38 +0100

    use debug() option, use identity-obj-proxy to show className in debugger

--- src/shared/__tests__/auto-scaling-text.js
+++ src/shared/__tests__/auto-scaling-text.js
@@ -2,6 +2,16 @@ import React from 'react'
 import {render} from '@testing-library/react'
 import AutoScalingText from '../auto-scaling-text'
 
-test('renders', () => {
-  render(<AutoScalingText />)
+describe('Run tests', () => {
+  test('renders AutoScalingText', () => {
+    render(<AutoScalingText />)
+  })
+  test('run debug', () => {
+    /* DESC The className prop doesn't show in the debugger because we replaced all .css files with an empty mocked module. 
+    /  for the class name to show in debugger, we have to install identity-obj-proxy module and update jest.config file.
+    /  The case occurs only if we use css-modules in our project. ClassName declared as string will appear normally.   
+    */
+    const {debug} = render(<AutoScalingText />)
+    debug()
+  })
 })
